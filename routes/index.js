0f20710d1ad9cd6f2f2fa9aa08599e6e18a98b85
Jervis Bay <jervis.bay@gmail.com>
2020-06-17 18:37:36 -0400

    changed workout to Workout, weird

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,44 @@
 // Use Router to route requests
 const router = require("express").Router();
 const path = require("path");
-// const Workout = require("../models/workout.js")
+const Workout = require("../models/Workout.js")
 
-// // database
-// const db = require("../models");
+// database
+const db = require("../models");
 
-// // api routes
-// router.get("/api/workouts", (req, res) => {
-//     // res.json all the workouts from workout collection
-//     Workout.find()
-//         .then(data => {
-//             res.json(data)
-//         })
-//         .catch(e => { res.json(e) })
-// })
+// api routes
+router.get("/api/workouts", (req, res) => {
+    // res.json all the workouts from workout collection
+    Workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(e => { res.json(e) })
+})
 
-// router.post("/api/workouts", (req, res) => {
-//     Workout.create({})
-//         .then(data => {
-//             res.json(data)
-//         })
-//         .catch(e => { console.log(e) })
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(data => {
+            res.json(data)
+        })
+        .catch(e => { console.log(e) })
 
-// })
+})
 
-// router.put("/api/workouts/:id", (req, res) => {
-//     Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
-//         .then(data => { res.json(data) })
-//         .catch(e => { res.json(e) })
-// })
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+        .then(data => { res.json(data) })
+        .catch(e => { res.json(e) })
+})
 
-// router.get("/api/workouts/range", (req, res) => {
-//     Workout.find({}).limit(7)
-//         .then(data => {
-//             console.log(data);
-//             res.json(data)
-//         })
-//         .catch(e => { console.log(e) })
-// })
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7)
+        .then(data => {
+            console.log(data);
+            res.json(data)
+        })
+        .catch(e => { console.log(e) })
+})
 
 
 
